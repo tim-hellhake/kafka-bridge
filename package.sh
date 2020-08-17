8c88ec653af294bb858bc774e1978db8704ac996
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2020-08-17 21:02:44 +0200

    Add cross-compile GitHub Action (#2)

--- package.sh
+++ package.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 
+# Setup environment for building inside Dockerized toolchain
+export NVM_DIR="${HOME}/.nvm"
+[ -s "${NVM_DIR}/nvm.sh" ] && source "${NVM_DIR}/nvm.sh"
+[ $(id -u) = 0 ] && umask 0
+
 npm ci
 npm run build
 rm -rf node_modules
@@ -10,25 +15,22 @@ else
   NODE_VERSION="$(node --version)"
   TARFILE_SUFFIX="-${ADDON_ARCH}-${NODE_VERSION/\.*/}"
 fi
-# For openwrt-linux-arm and linux-arm we need to cross compile.
-if [[ "${ADDON_ARCH}" =~ "linux-arm" ]]; then
-  # We assume that CC and CXX are pointing to the cross compilers
-  npm ci --ignore-scripts --production
-  npm rebuild --arch=armv6l --target_arch=arm
-else
-  npm ci --production
-fi
+
+npm ci --production
+
+shasum --algorithm 256 manifest.json package.json lib/*.js LICENSE README.md > SHA256SUMS
 
 rm -rf node_modules/.bin
-
-shasum --algorithm 256 package.json manifest.json lib/*.js LICENSE README.md > SHA256SUMS
 find node_modules \( -type f -o -type l \) -exec shasum --algorithm 256 {} \; >> SHA256SUMS
+
 TARFILE=`npm pack`
+
 tar xzf ${TARFILE}
 rm ${TARFILE}
 TARFILE_ARCH="${TARFILE/.tgz/${TARFILE_SUFFIX}.tgz}"
 cp -r node_modules ./package
 tar czf ${TARFILE_ARCH} package
+
 shasum --algorithm 256 ${TARFILE_ARCH} > ${TARFILE_ARCH}.sha256sum
-rm -rf package
-echo "Created ${TARFILE_ARCH}"
+
+rm -rf SHA256SUMS package
