b46c4c871818a31949e9fef34faa0ca60186a172
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2020-12-12 23:58:45 +0100

    Update to new webthings library

--- src/kafka-bridge.ts
+++ src/kafka-bridge.ts
@@ -6,13 +6,7 @@
 
 import { Adapter, } from 'gateway-addon';
 import { WebThingsClient } from 'webthings-client';
-import { client as WebSocketClient } from 'websocket';
 import { Producer, KafkaClient, ProduceRequest } from 'kafka-node';
-
-interface ThingEvent {
-    messageType: string,
-    data: {}
-}
 
 export class KafkaBridge extends Adapter {
     constructor(addonManager: any, private manifest: any) {
@@ -49,62 +43,20 @@ export class KafkaBridge extends Adapter {
 
         (async () => {
             const webThingsClient = await WebThingsClient.local(accessToken);
-            const devices = await webThingsClient.getDevices();
+            await webThingsClient.connect();
+            webThingsClient.on('propertyChanged', async (deviceId, key, value) => {
+                const messages: ProduceRequest[] = [{
+                    topic: deviceId,
+                    key,
+                    messages: value
+                }];
 
-            for (const device of devices) {
-                try {
-                    const parts = device.href.split('/');
-                    const deviceId = parts[parts.length - 1];
-
-                    console.log(`Connecting to websocket of ${deviceId}`);
-                    const thingUrl = `ws://localhost:8080${device.href}`;
-                    const webSocketClient = new WebSocketClient();
-
-                    webSocketClient.on('connectFailed', function (error) {
-                        console.error(`Could not connect to ${thingUrl}: ${error}`)
-                    });
-
-                    webSocketClient.on('connect', function (connection) {
-                        console.log(`Connected to ${thingUrl}`);
-
-                        connection.on('error', function (error) {
-                            console.log(`Connection to ${thingUrl} failed: ${error}`);
-                        });
-
-                        connection.on('close', function () {
-                            console.log(`Connection to ${thingUrl} closed`);
-                        });
-
-                        connection.on('message', function (message) {
-                            if (message.type === 'utf8' && message.utf8Data) {
-                                const thingEvent = <ThingEvent>JSON.parse(message.utf8Data);
-
-                                if (thingEvent.messageType === 'propertyStatus') {
-                                    const messages: ProduceRequest[] = [];
-
-                                    for (const [key, value] of Object.entries(thingEvent.data)) {
-                                        messages.push({
-                                            topic: deviceId,
-                                            key,
-                                            messages: value
-                                        });
-                                    }
-
-                                    producer.send(messages, (err, _data) => {
-                                        if (err) {
-                                            console.log(`Could not send message: ${err}`);
-                                        }
-                                    });
-                                }
-                            }
-                        });
-                    });
-
-                    webSocketClient.connect(`${thingUrl}?jwt=${accessToken}`);
-                } catch (e) {
-                    console.log(`Could not process device ${device.title} ${e}`);
-                }
-            }
+                producer.send(messages, (err, _data) => {
+                    if (err) {
+                        console.log(`Could not send message: ${err}`);
+                    }
+                });
+            });
         })();
     }
 }
